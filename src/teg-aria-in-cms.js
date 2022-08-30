c0f345c83ae3d1489fe8592304b312d1f24c6815
Paul B. Joiner <paulj@engageyourcause.com>
2022-08-30 13:35:28 -0500

    fix check for existing ID

--- src/teg-aria-in-cms.js
+++ src/teg-aria-in-cms.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
 	/**
 	 * Pull a collection of values from a set of CSS class name prefixes.
 	 *
@@ -26,42 +26,41 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	 * Generate ID from value specified by class name only if that item is unique.
 	 */
 	document.querySelectorAll('[class*="aic-id-"]')
-			.forEach((thisNode) => {
-				thisNode.classList
-						.forEach((thisClass) => {
+	        .forEach((thisNode) => {
+		        thisNode.classList
+		                .forEach((thisClass) => {
 
-							if (thisClass.indexOf('aic-id-') === 0) {
-								const thisID = thisClass.substring(7);
+			                if (thisClass.indexOf('aic-id-') === 0) {
+				                const thisID = thisClass.substring(7);
 
-								if (document.querySelector(`#${thisID}`) !== null) {
-									thisNode.id = thisID;
-								}
-							}
-						});
-			});
+				                if (document.querySelector(`#${thisID}`) === null) {
+					                thisNode.id = thisID;
+				                }
+			                }
+		                });
+	        });
 
 	// wrap bits of CMS generated HTML in ARIA elements
 	let wrappedItems = {};
 
 	document.querySelectorAll('[class*="aic-wrap-"]')
 	        .forEach((thisNode) => {
-		        let wrapAttributes = getARIAAttributes(thisNode, ['aic-wrap-', 'aic-wrap-label-', 'aic-wrap-labelledby-']),
-		            wrappedItemKey = wrapAttributes['aic-wrap-label-'] !== '' ? wrapAttributes['aic-wrap-label-'] : wrapAttributes['aic-wrap-labelledby-'];
+		        let wrapAttributes = getARIAAttributes(thisNode, ['aic-wrap-item-', 'aic-wrap-label-', 'aic-wrap-labelledby-']),
+		            wrappedItemKey = typeof wrapAttributes['aic-wrap-label-'] !== 'undefined' ? wrapAttributes['aic-wrap-label-'] : wrapAttributes['aic-wrap-labelledby-'];
 
 		        if (typeof wrappedItems[wrappedItemKey] === "undefined") {
 			        wrappedItems[wrappedItemKey] = {
-				        wrappedItemKey: {
-					        wrapElement   : wrapAttributes['aic-wrap-'],
-					        wrapLabel     : wrapAttributes['aic-wrap-label'],
-					        wrapLabelledBy: wrapAttributes['aic-wrap-labelledby-'],
-					        itemList      : [thisNode],
-				        }
+				        wrapElement   : wrapAttributes['aic-wrap-item-'],
+				        wrapLabel     : wrapAttributes['aic-wrap-label-'] ? wrapAttributes['aic-wrap-label-'].replace(/[-_]/g, ' ') : '',
+				        wrapLabelledBy: wrapAttributes['aic-wrap-labelledby-'],
+				        itemList      : [thisNode],
 			        };
 
 		        } else {
 			        wrappedItems[wrappedItemKey].itemList.push(thisNode);
 		        }
 	        });
+
 	for (let wrappedItemsKey in wrappedItems) {
 		const newNode = document.createElement(wrappedItems[wrappedItemsKey].wrapElement);
 
@@ -75,7 +74,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 			}
 		}
 
-		wrappedItems[wrappedItemsKey].itemList[0].parent.insertBefore(newNode, wrappedItems[wrappedItemsKey].itemList[0]);
+		wrappedItems[wrappedItemsKey].itemList[0].parentElement.insertBefore(newNode, wrappedItems[wrappedItemsKey].itemList[0]);
 		newNode.append(wrappedItems[wrappedItemsKey].itemList[0]);
 
 		for (let counter = 1; counter < wrappedItems[wrappedItemsKey].itemList.length; counter++) {
@@ -86,12 +85,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	// set the role attribute
 	document.querySelectorAll('[class*="aic-role-"]')
 	        .forEach((thisNode) => {
-		        let roleAttributes = getARIAAttributes(thisNode, ['aic-role-', 'aic-role-label-', 'aic-role-labelledby-']);
+		        let roleAttributes = getARIAAttributes(thisNode, ['aic-role-item-', 'aic-role-label-', 'aic-role-labelledby-']);
 
-		        thisNode.setAttribute('role', roleAttributes['aic-role-']);
+		        thisNode.setAttribute('role', roleAttributes['aic-role-item-']);
 
 		        if (roleAttributes['aic-role-label-'] !== '') {
-			        thisNode.setAttribute('aria-label', roleAttributes['aic-role-label-']);
+			        thisNode.setAttribute('aria-label', roleAttributes['aic-role-label-'].replace(/[-_]/g, ' '));
 
 		        } else {
 
